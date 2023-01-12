ed9871164c50f89a4dd41af4558ff8cf97841a6a
Corey <corey@mk-gang-imac.lan>
2023-01-11 22:22:49 -0500

    updates to code for assignment submission

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,14 +26,14 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We define a variable 'Dane'.
+// We'll look for the variable Dane.
+// If the Variable is Mary we'll print Hi, Mary! in console
+// If it is not Mary, we'll print How do you do? in the absence of defined variable. 
 
 //CODE
 /*
-let name = 'Dane';
+let name = 'Dane'; //What happens if Dane is True?
 
 if (name === 'Mary') {
   console.log('Hi, Mary!');
@@ -45,10 +45,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// define variable secret as a variable option. A precursor
+// define variable 'code' as 123 int
+// if code is 123 then define secret as 'super' then change the variable 'code' to 2 times it's value. 123*2= 246
+// if code now is greater than 250 define the variable 'secret' as 'duper'; it is not greater than 250 (246<250)
+// in this case we will only print 'super' in the console.
 
 //CODE
 /*
@@ -70,10 +71,14 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// defining three variables. isStudent, age, zip
+// isStudent is defined as boolean 'true'
+// age is defined as int 34
+// zip is defined as int 55407
+// look to see if isStudent is true AND zip is greater than 80000. if it is we'll log 'you're a student on the West Coast.' in the console. isStudent=true is true, zip is not. Move on.
+// look now to see if isStudent is false OR the age is less than 30. if either of those statements are true then we print 'What are your hobbies?' in the console. isStudent=false is false but we have an OR statement so we can move to look to see if age is less than 30. it is not. move on.
+// now we'll look to see if isStudent is true. isStudent=true so this is true. in this case we'll print 'Welcome to Prime!' in the log.
+// if none of these were true, we would print 'How about the weather?' in the console. We have one true statement so we won't print this. 
 
 //CODE
 /*
@@ -130,12 +135,13 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
-let colorOne = 'red';
-let colorTwo = 'blue';
-let mix = true;
+let colorOne = 'blue'; // changed red to blue line 138
+let colorTwo = 'red'; // changed blue to red line 139
+let mix = true; //correct
 
 if (mix === true) {
   colorOne = 'purple';
+  colorTwo = 'purple'; // added line 144 so colorTwo will change to purple
 }
 */
 
@@ -145,11 +151,12 @@ if (mix === true) {
 
 /*
 let temp = 40;
-const time = 4;
+let time = 4; //changed 'time' to be a variable from a constant 'const--->let' line 154
 
-if (temp > 39 || time >= 4) {
+if (temp > 39 && time >= 4) {
   console.log('throw away the food!');
 }
+// changed from OR to AND line 156 '||--->&&
 */
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
@@ -158,12 +165,14 @@ if (temp > 39 || time >= 4) {
 
 /*
 let age = 21;
-const minAge = 21;
+let minAge = 21; //change minAge from constant to variable 'const--->let' line 168
 
-if(minAge <= age) {
+if(minAge >= age) {
   console.log('no entry');
 } else {
   console.log('enter');
 }
+// changed <= to >= line 170
+// line 171 could be removed as it wasn't in the instruction but the code works as intended since console.log will print 'enter' give minAge is equal to age
 */
 
